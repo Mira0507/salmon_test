125c6d122e0b4eb0aa2221effc02b48c881b3115
Mira Sohn <tonton07@gmail.com>
2020-10-29 00:22:49 -0400

    salmon execution bash file

--- singlequant.sh
+++ singlequant.sh
@@ -4,7 +4,7 @@ cd rawdata
 
 for read in $(ls) 
 do
-    salmon quant -i ../salmon_sa_index_hg19 -l A -r $read -p 8 --validateMappings -o ../${read}.salmon_quant
+    salmon quant -i ../salmon_sa_index_hg19 -l A --gcBias --seqBias -r $read -p 8 --validateMappings -o ../${read}.salmon_quant
 done
 
 cd ..
